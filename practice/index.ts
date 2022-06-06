926d8d11ca61426d6f78ea0286b01f910d558e66
Ayasono <yuehu@ualberta.ca>
2022-06-06 23:52:35 +0800

    index.ts with subscribers

--- practice/index.ts
+++ practice/index.ts
@@ -1,7 +1,40 @@
-enum scores {
-    excellent = 100,
-    good = 80,
-    average = 50,
-    fail = 30
+class MisakaNet {
+    sisters: Sister[]
+    constructor() {
+        this.sisters = []
+    }
+    pushMsg (msg: string) {
+        this.sisters.forEach(sister => {
+            sister.receiveMsg(msg)
+        })
+    }
 }
-const tuple: Array<any> = [1, "string", true];
+
+class Sister {
+    name: string
+    constructor(name: string) {
+        this.name = name
+    }
+    subscribeNet (MisakaNet: MisakaNet): Sister {
+        MisakaNet.sisters.push(this)
+        return this
+    }
+    useNet (MisakaNet: MisakaNet, msg: string) {
+        MisakaNet.pushMsg(msg)
+    }
+    receiveMsg (msg: string) {
+        console.log(`Misaka${this.name} received: ` + msg)
+    }
+}
+
+const misakaNet = new MisakaNet()
+for (let i = 0; i < 100; i ++) {
+    new Sister(`${i + 1}`)
+        .subscribeNet(misakaNet)
+        .useNet(misakaNet, `Hello, I am sister #${i + 1}`)
+}
+
+console.log(misakaNet.sisters)
+misakaNet
+    .sisters[0]
+    .useNet(misakaNet, 'Hello, MisakaMikoto')
